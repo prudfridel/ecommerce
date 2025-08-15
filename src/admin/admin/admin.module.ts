93bb7df6f96ff0a265be337a06fef29954f06b23
prudfridel <prudefridel@gmail.com>
2025-08-15 16:21:13 +0100

    .

--- src/admin/admin/admin.module.ts
+++ src/admin/admin/admin.module.ts
@@ -15,7 +15,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         secret:configService.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: '1d' },
+        signOptions: { expiresIn: undefined },
       }),
     }),
   ],
